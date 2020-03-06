8c43dce864c084677a0ce7eb1e5bc80cafa1fa39
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-03-06 09:16:09 -0300

    initial navbar

---
+++ src/components/initial/nav-initial.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav>
+    <img :src="logoSrc" id="logo">
+    <div id="inputs">
+      <input name="email" id="email" type="text" placeholder="Endereço de email" />
+      <input name="senha" id="senha" type="text" placeholder="Senha" />
+      <button id="btn"><b>Entrar</b></button>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "nav-initial",
+  data() {
+    return {
+      logoSrc: "./src/assets/logo2.PNG",
+    };
+  }
+};
+</script>
+<style>
+nav {
+    position: absolute;
+    top: 0;
+    right: 0;
+  padding: 0.3%;
+  height: 60.3px;
+  background-color: white;
+  width:99.9%;
+  background-color: #f9e0008f;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+#logo{
+    width: 320px;
+    height: 70px;
+    margin-bottom: -15px;
+}
+
+#inputs{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 3%;
+}
+
+#btn{
+    padding: 0 3.8%;
+    text-align: center;
+    background-color: #1FC468;
+    border-style: none;
+    color:white;
+    border-radius: 3px;
+    height: 27px;
+    width: 100%;
+}
+
+input{
+    text-indent: 10px;
+    border-radius: 3px;
+    height: 27px;
+    border-style: none;
+    margin-right: 2.7%;
+}
+</style>
