d1b55c51017472128d6d1824bfa92e8a92931a0c
Nouani <nouanigabriel@hotmail.com>
2020-03-30 22:06:15 -0300

    Refatoração do select

--- src/views/signup/Signup.vue
+++ src/views/signup/Signup.vue
@@ -5,7 +5,7 @@
       <div class="input-container">
         <div class="file-group">
           <label for="seletorArquivo">Icone de Conta aqui</label>
-          <input type="file" name="seletorArquivo" id="seletorArquivo"/>
+          <input type="file" name="seletorArquivo" id="seletorArquivo" />
         </div>
         <div class="input-group">
           <input class="margin-style" type="text" name="nome" placeholder="Nome" />
@@ -16,12 +16,14 @@
           <input class="margin-style" type="password" name="senha" placeholder="Senha" />
           <input type="password" name="confirma" placeholder="Confirme a senha" />
         </div>
-        <SelectPolitico :url="urlDeputados" :text="'Selecione o deputado em que você votou!'" />
-        <SelectPolitico :url="urlSenadores" :text="'Selecione o senador em que você votou!'" />
+        <div class="select-group">
+          <SelectPolitico :url="urlDeputados" :text="'Selecione o deputado em que você votou!'" />
+          <SelectPolitico :url="urlSenadores" :text="'Selecione o senador em que você votou!'" />
+        </div>
       </div>
       <div class="button-group">
-        <button type="button" id="button-haveAccount" >Já tenho uma conta</button>
-        <button type="button" id="button-register" >Cadastrar</button>
+        <button type="button" id="button-haveAccount">Já tenho uma conta</button>
+        <button type="button" id="button-register">Cadastrar</button>
       </div>
     </form>
   </div>
