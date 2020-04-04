94cc42207a878427c4219873fdb0448c5a512d83
Nouani <nouanigabriel@hotmail.com>
2020-04-03 21:06:08 -0300

    Responsividade do classification

--- src/components/classification/Classification.vue
+++ src/components/classification/Classification.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="container">
-        <Search :size="8" class="search" />
-    </div>
+  <div class="container" :class="{ 'move': isOpen }">
+    <button :class="{ 'show': !mobileView }" @click="showNav">
+      <i class="fas fa-bars"></i>
+    </button>
+    <Search :size="8" class="search" />
+  </div>
 </template>
 <script>
 import Search from '../search/Search.vue';
@@ -10,11 +13,26 @@ export default {
   name: 'Classification',
   data() {
     return {
-
+      mobileView: false,
+      isOpen: false,
     };
   },
   components: {
     Search,
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 1125;
+      console.log(this.mobileView);
+    },
+    showNav() {
+      this.isOpen = !this.isOpen;
+      console.log(this.isOpen);
+    },
+  },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
   },
 };
 </script>
