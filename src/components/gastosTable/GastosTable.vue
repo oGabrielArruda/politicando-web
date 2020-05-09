58e605e985d938a5a3d7a7b888570103e8e3fd23
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-05-09 15:15:42 -0300

    refatoracao gastos table

--- src/components/gastosTable/GastosTable.vue
+++ src/components/gastosTable/GastosTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="gastos-table">
     <select v-model="anoEscolhido">
-      <option selected>Todos os anos</option>
+      <option selected value=0>Todos os anos</option>
       <option :key="ano" v-for="ano in anos">{{ ano }}</option>
     </select>
     <VueGoodTable
@@ -40,28 +40,26 @@ export default {
     };
   },
   methods: {
-    async addPolitico(politico) {
-      const gastos = await api.get(`/PoliticoItems/${politico.id}/gastos`);
-      const somatoria = this.getSomatoria(gastos.data);
-      this.linhas.push({ nome: politico.nome, gastos: somatoria });
+    async addPoliticos(politicosArr) {
+      politicosArr.forEach(async (t) => {
+        const responseGastos = await api.get(`/PoliticoItems/${t.id}/gastos`);
+        const gastosFiltrados = this.filtrarPorAno(responseGastos.data, this.anoEscolhido);
+        const somatoria = this.getSomatoria(gastosFiltrados);
+        this.adicionarLinhaNaTabela(t.nome, somatoria);
+      });
     },
-    async addPoliticoPorAno(politico, ano) {
-      const responseGastos = await api.get(
-        `/PoliticoItems/${politico.id}/gastos`,
-      );
-      let gastos = responseGastos.data;
-
+    filtrarPorAno(gastos, ano) {
       const anoInt = parseInt(ano, 10);
-      if (ano !== 'Todos os anos') {
-        gastos = gastos.filter((t) => t.ano === anoInt);
-      }
-      const somatoria = this.getSomatoria(gastos);
-
-      this.linhas.push({ nome: politico.nome, gastos: somatoria });
+      if (anoInt === 0) { return gastos; }
+      const gastosFiltrados = gastos.filter((t) => t.ano === anoInt);
+      return gastosFiltrados;
     },
     getSomatoria(gastos) {
       const somatoria = gastos.reduce((prev, { valor }) => prev + valor, 0);
       return somatoria;
+    },
+    adicionarLinhaNaTabela(nome, gastos) {
+      this.linhas.push({ nome, gastos });
     },
     formatFn(value) {
       const valInt = parseInt(value, 10);
@@ -73,29 +71,16 @@ export default {
     },
   },
   async mounted() {
-    this.politicos.forEach(async (t) => {
-      this.addPolitico(t);
-    });
+    this.addPoliticos(this.politicos);
   },
   watch: {
-    async politicos() {
+    async politicos(politicosArr) {
       this.linhas = [];
-      console.log(this.anoEscolhido);
-      if (this.anoEscolhido === 0) {
-        this.politicos.forEach(async (t) => {
-          this.addPolitico(t);
-        });
-      } else {
-        this.politicos.forEach(async (t) => {
-          this.addPoliticoPorAno(t, this.anoEscolhido);
-        });
-      }
+      this.addPoliticos(politicosArr);
     },
     anoEscolhido() {
       this.linhas = [];
-      this.politicos.forEach(async (t) => {
-        this.addPoliticoPorAno(t, this.anoEscolhido);
-      });
+      this.addPoliticos(this.politicos);
     },
   },
   computed: {
