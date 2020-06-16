7210749928c70e2b56eb719f0abee453f3613988
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-06-16 13:30:41 -0300

    sending faq messages

--- src/views/contato/Contato.vue
+++ src/views/contato/Contato.vue
@@ -5,14 +5,14 @@
             <div class="message-area">
                <h1> Nos mande uma mensagem! </h1>
                 <textarea v-model="texto" />
-                <select>
+                <select v-model="tipo">
                     <option value=0> Escolha o tipo da mensagem </option>
                     <option :key="opt" v-for="opt in options"> {{ opt }} </option>
                 </select>
             </div>
             <div class="info-area">
                 <input placeholder="Digite seu email" v-model="email"/>
-                <button @click="sendMessage"> Enviar </button>
+                <button @click="handleSubmit"> Enviar </button>
             </div>
         </div>
         <Footer />
@@ -22,6 +22,7 @@
 <script>
 import Navbar from '../../components/navbar/Navbar.vue';
 import Footer from '../../components/footer/Footer.vue';
+import api from '../../config/api';
 
 export default {
   name: 'Contato',
@@ -29,7 +30,8 @@ export default {
     return {
       texto: '',
       email: '',
-      options: ['Sugestão', 'Dúvidas', 'Problemas encontrados'],
+      tipo: '',
+      options: ['Sugestão', 'Dúvidas', 'Problemas encontrados', 'Outro'],
     };
   },
   components: {
@@ -37,26 +39,41 @@ export default {
     Footer,
   },
   methods: {
-    sendMessage(e) {
+    async handleSubmit(e) {
       e.preventDefault();
+      const messageObj = this.getMessageObject();
+      await this.sendMessage(messageObj);
       this.texto = '';
-      /* Email.send({
-        SecureToken: '6d86335f-f6a9-458c-a797-9854288c0ea1',
-        To: 'gabrielarruda1@cooltura.com.br',
-        From: this.email,
-        Subject: 'This is the subject',
-        Body: 'And this is the body',
-      }).then(
-        (message) => console.log(message),
-      );
-    }, */
+      this.email = '';
     },
-  },
-  mounted() {
-    const smtpScript = document.createElement('script');
-    smtpScript.setAttribute('src', 'https://smtpjs.com/v3/smtp.js');
-    document.head.appendChild(smtpScript);
-    smtpScript.async = true;
+    getMessageObject() {
+      const date = this.generateDate();
+      const obj = {
+        id: 0,
+        email: this.email,
+        tipo: this.tipo,
+        data: date,
+        mensagem: this.texto,
+        statusF: 'NÃO RESOLVIDA',
+      };
+      return obj;
+    },
+    generateDate() {
+      let today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0');
+      const yyyy = today.getFullYear();
+      today = `${dd}/${mm}/${yyyy}`;
+      return today;
+    },
+    async sendMessage(messageObj) {
+      try {
+        console.log(messageObj);
+        await api.post('/Faq/send', messageObj);
+      } catch (erro) {
+        console.log(erro);
+      }
+    },
   },
 };
 
